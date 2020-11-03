b2301e9a11200287f1df0251b63352ef1ac22755
Dimitri Mudiangondo <dimitrimudi@gmail.com>
2020-11-03 18:39:24 +0100

    ok

--- controllers/user.js
+++ controllers/user.js
@@ -51,3 +51,17 @@ exports.login = (req, res, next) =>
     })
     .catch(error => res.status(500).json({ error }))
 }
+
+exports.getAll = (req, res, next)=>{
+    User.find().then(
+        (users) => {
+            res.status(200).json(users);
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+      );
+}
--- routers/stuff.js
+++ routers/stuff.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 router.post('/', auth, multer, stuffCtrl.createThing);
-router.get('/', auth, stuffCtrl.getAllStuff);
+router.get('/', stuffCtrl.getAllStuff);
 router.put('/:id', auth, stuffCtrl.modifyThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
