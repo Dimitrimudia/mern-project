11957499e8c2fd796ea9a8b8dc968adf47b5b06b
Dimitri Mudiangondo <dimitrimudia@gmail.com>
2020-11-05 20:13:31 +0100

    commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routers/stuff');
 const userRoutes = require('./routers/user');
@@ -21,13 +22,7 @@ db.once('open', _=> {
 db.on('err', err =>{
     console.log('connection error :', err)
 });
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
